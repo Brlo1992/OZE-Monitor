675e5d5f9e332e96f854acf7fb5742cc6dd90dfe
dawid krzus <dkrzus92@gmail.com>
2018-02-25 01:22:19 +0100

    Add testing endpoint

---
+++ OZE.WebApi/OZE.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OZE.WebApi.Controllers {
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Get() {
+            return Ok("Hello World");
+        }
+    }
+}
